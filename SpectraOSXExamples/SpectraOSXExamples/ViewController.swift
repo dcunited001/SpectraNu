2e5ec90763f59b63b1fa40854ceb44da6e2218ad
David Conner <dconner.pro@gmail.com>
2015-11-30 15:41:29 -0500

    Example projects: added iOS & OSX projects
    * started configuring the SpectraiOS & SpectraOSX frameworks to be linked in

---
+++ SpectraOSXExamples/SpectraOSXExamples/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpectraOSXExamples
+//
+//  Created by David Conner on 11/30/15.
+//  Copyright © 2015 Spectra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ SpectraiOSExamples/SpectraiOSExamples/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SpectraiOSExamples
+//
+//  Created by David Conner on 11/30/15.
+//  Copyright © 2015 Spectra. All rights reserved.
+//
+
+import UIKit
+import SpectraiOS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
