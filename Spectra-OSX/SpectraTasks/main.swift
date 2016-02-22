5e37dc82507de8f03a43f4b23ab8a020cc9a62d4
David Conner <dconner.pro@gmail.com>
2016-02-22 08:02:53 -0500

    SpectraTasks: add task for autogeneration of bundle resources

---
+++ Spectra-OSX/SpectraTasks/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  SpectraTasks
+//
+//  Created by David Conner on 2/22/16.
+//  Copyright © 2016 Spectra. All rights reserved.
+//
+
+import Foundation
+
+let currentDir = NSTemporaryDirectory()
+
+print(NSTemporaryDirectory())
+
+print("done")
+
