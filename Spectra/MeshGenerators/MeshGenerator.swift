461962105210f881e2d673540cda33d495789fc6
David Conner <dconner.pro@gmail.com>
2016-03-04 04:05:52 -0500

    SpectraGeo: add helpders for Spectra Geometry functionality

--- Spectra/MeshGenerators/MeshGenerator.swift
+++ Spectra/MeshGenerators/MeshGenerator.swift
@@ -9,10 +9,46 @@
 import Metal
 import ModelIO
 import simd
+import Swinject
+
+// NOTE: trying to decouple SpectraGeo from the rest of Spectra
+// - or at least parts of it
+
+public class SpectraGeo {
+    
+    public static func defaultVertexDescriptor() -> MDLVertexDescriptor {
+        // this vertex desc can be replaced,
+        // - but this descriptor describes the data layout
+        
+        let attrPos = MDLVertexAttribute(name: MDLVertexAttributePosition, format: .Float4, offset: 0, bufferIndex: 0)
+        let attrColor = MDLVertexAttribute(name: MDLVertexAttributeColor, format: .Int4, offset: 0, bufferIndex: 1)
+        let attrTex = MDLVertexAttribute(name: MDLVertexAttributeTextureCoordinate, format: .Float4, offset: 0, bufferIndex: 2)
+        let attrNorm = MDLVertexAttribute(name: MDLVertexAttributeNormal, format: .Float4, offset: 0, bufferIndex: 3)
+        
+        let desc = MDLVertexDescriptor()
+        desc.addOrReplaceAttribute(attrPos)
+        desc.addOrReplaceAttribute(attrColor)
+        desc.addOrReplaceAttribute(attrTex)
+        desc.addOrReplaceAttribute(attrNorm)
+        
+        return desc
+    }
+    
+    // TODO: move this elsewhere
+
+//    public static func getClassForVertexFormat(vertexFormat: MDLVertexFormat) -> Any {
+//        switch vertexFormat {
+        // TODO: i had hoped to use return the class here, then reference this in the generateData() method
+        // - to infer type on a variable that would receive the value from the generic generateAttribute() method
+        // - but i can't really do much with structs =/
+//        }
+//    }
+    
+}
 
 public protocol MeshGenerator {
     //    func flattenMap(vertexMap: OrderedDictionary<Int, [Int]>) -> [Int]
-    func generate(args: [String: AnyObject]) -> MDLMesh
+    func generate(container: Container, args: [String: GeneratorArg]) -> MDLMesh
     
 //    func getVertices() -> [float4]
 //    func getColorCoords() -> [float4]
