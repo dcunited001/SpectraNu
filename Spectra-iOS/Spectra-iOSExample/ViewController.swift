4d1a458fe7560d56d03dd462bd16c7a02755777d
David Conner <dconner.pro@gmail.com>
2016-03-06 06:25:11 -0500

    fix similar problems with running on iOS

--- Spectra-iOS/Spectra-iOSExample/ViewController.swift
+++ Spectra-iOS/Spectra-iOSExample/ViewController.swift
@@ -9,14 +9,27 @@
 import UIKit
 import Spectra
 import Fuzi
+import Swinject
 
 class ViewController: UIViewController {
+    
+    var device: MTLDevice?
+    var library: MTLLibrary?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Spectra.foo()
+        self.device = MTLCreateSystemDefaultDevice()
+        self.library = device!.newDefaultLibrary()
+        
+        // Do any additional setup after loading the view.
+        let appBundle = NSBundle(forClass: ViewController.self)
+        let xmlData: NSData = S3DXML.readXML(appBundle, filename: "DefaultPipeline")
+        let s3d = S3DXML(data: xmlData)
+        
+        var descMan = DescriptorManager(library: library!)
+        descMan.parseS3DXML(s3d)
     }
 
     override func didReceiveMemoryWarning() {
