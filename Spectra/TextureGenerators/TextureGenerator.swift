f66c0d9ccd40547013a8d7e84b6812f197ef574e
David Conner <dconner.pro@gmail.com>
2016-03-05 20:38:07 -0500

    adding TextureGenerator protocol (it'll be a minute before there are any implementations, methinks)

---
+++ Spectra/TextureGenerators/TextureGenerator.swift
@@ -0,0 +1,15 @@
+//
+//  TextureGenerator.swift
+//  
+//
+//  Created by David Conner on 3/5/16.
+//
+//
+
+import Foundation
+
+public protocol TextureGenerator {
+    init(container: Container, args: [String: GeneratorArg])
+    func generate(container: Container, args [String: GeneratorArg])
+}
+
