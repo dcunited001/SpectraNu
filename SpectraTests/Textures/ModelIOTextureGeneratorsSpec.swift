db98f4c20325e459d8924ef32eb7928ac0fabf36
David Conner <dconner.pro@gmail.com>
2016-03-08 14:40:34 -0500

    tests for ModelIO texture generator

--- SpectraTests/Textures/ModelIOTextureGeneratorsSpec.swift
+++ SpectraTests/Textures/ModelIOTextureGeneratorsSpec.swift
@@ -12,26 +12,99 @@ import Quick
 import Nimble
 import ModelIO
 
+// TODO: test overriding args in generate()
+
 class ModelIOTextureGeneratorsSpec: QuickSpec {
     
-    
     override func spec() {
+        let parser = Container()
         
+        let spectraEnumData = SpectraXSD.readXSD("SpectraEnums")
+        let spectraEnumXSD = SpectraXSD(data: spectraEnumData)
+        spectraEnumXSD.parseEnumTypes(parser)
+        
+        let testBundle = NSBundle(forClass: ModelIOTextureGeneratorsSpec.self)
+        let xmlData: NSData = SpectraXML.readXML(testBundle, filename: "ModelIOTextureGenerators")
+        let assetContainer = Container(parent: parser)
+        
+        ModelIOTextureGenerators.loadTextureGenerators(assetContainer)
+        
+        SpectraXML.initParser(parser)
+        let spectraXML = SpectraXML(parser: parser, data: xmlData)
+        spectraXML.parse(assetContainer, options: [:])
         
         describe("resource_texture") {
+            let texGen = assetContainer.resolve(TextureGenerator.self, name: "resource_tex_gen")
             
+            it("can override generator defaults") {
+                expect(texGen.name) == "defaultTexture.jpg"
+            }
+            
+            it("creates meshes") {
+                let tex = assetContainer.resolve(MDLTexture.self, name: "resource_tex")
+                print(tex)
+            }
         }
         
-        describe("resource_cube_texture") {
-            
-        }
+        // TODO:  describe("resource_cube_texture") { }
         
         describe("url_texture") {
+            let texGen = assetContainer.resolve(TextureGenerator.self, name: "url_tex_gen2")
             
+            it("can override generator defaults") {
+                // TODO: manually set texture URL on generator using $(PROJECT_PATH)
+                expect(texGen.url) == "file:///itsafile.jpg"
+            }
         }
         
-        describe("noise_texture") {
+        describe("vector_noise_texture") {
+            let texGen = assetContainer.resolve(TextureGenerator.self, name: "vector_noise_gen")
             
+            it("can override generator defaults") {
+                // TODO: more validations
+                expect(texGen.type) == NoiseTextureGenType.Vector
+                expect(texGen.dimensions[0]) == 10
+                expect(texGen.dimensions[1]) == 10
+            }
+            
+            it("creates meshes") {
+                let tex = assetContainer.resolve(MDLTexture.self, name: "vector_noise_tex")
+                expect(tex.dimensions[0]) == 10
+                expect(tex.dimensions[1]) == 10
+            }
         }
+        
+        describe("scalar_noise_texture") {
+            let texGen = assetContainer.resolve(TextureGenerator.self, name: "scalar_noise_gen")
+            
+            it("can override generator defaults") {
+                // TODO: more validations
+                expect(texGen.type) == NoiseTextureGenType.Scalar
+                expect(texGen.dimensions[0]) == 10
+                expect(texGen.dimensions[1]) == 10
+            }
+            
+            it("creates meshes") {
+                let tex = assetContainer.resolve(MDLTexture.self, name: "scalar_noise_gen")
+                expect(tex.dimensions[0]) == 10
+                expect(tex.dimensions[1]) == 10
+            }
+        }
+        
+        describe("checkerboard_texture_gen") {
+            let texGen = assetContainer.resolve(TextureGenerator.self, name: "scalar_noise_gen")
+            
+            it("can override generator defaults") {
+                expect(texGen.divisions) == 19
+                expect(texGen.dimensions[0]) == 19
+                expect(texGen.dimensions[1]) == 19
+            }
+        }
+        
+        // TODO: describe("data_texture") { }
+        // TODO: describe("color_swatch_texture") { }
+        // TODO: describe("normal_map_texture") { }
+        // TODO: describe("sky_cube_texture") { }
+        
     }
 }
